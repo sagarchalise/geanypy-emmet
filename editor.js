0a18bb4b33e85cd2f331bc86b6f860bb040db19d
Sagar Chalise <chalisesagar@gmail.com>
2013-07-31 08:20:19 +0545

    Initial Emmet Plugin codebase

---
+++ editor.js
@@ -0,0 +1,243 @@
+function getScintilla() {
+	return cur_doc.editor.scintilla;
+}
+
+var editorProxy = emmet.exec(function(require, _) {
+	return {
+		getSelectionRange: function() {
+			var scintilla = getScintilla();
+			return {
+				start: scintilla.get_selection_start(),
+                end: scintilla.get_selection_end()
+			};
+		},
+
+		createSelection: function(start, end) {
+			var scintilla = getScintilla();
+            scintilla.set_selection_start(start);
+            scintilla.set_selection_end(end);
+		},
+
+		getCurrentLineRange: function() {
+			var scintilla = getScintilla();
+            var line = scintilla.get_current_line();
+			return {
+				start: scintilla.get_position_from_line(line),
+				end: scintilla.get_line_end_position(line)
+			};
+		},
+
+		getCaretPos: function() {
+			var scintilla = getScintilla();
+			return scintilla.get_current_position();
+		},
+
+		setCaretPos: function(pos){
+            var scintilla = getScintilla();
+			scintilla.set_current_position(pos);
+		},
+
+		getCurrentLine: function() {
+			var scintilla = getScintilla();
+			return scintilla.get_current_line();
+		},
+
+		replaceContent: function(value, start, end, noIndent) {
+            
+			if (_.isUndefined(end))
+				end = _.isUndefined(start) ? this.getContent().length : start;
+			if (_.isUndefined(start)) start = 0;
+
+			// update tabstops: make sure all caret placeholder are unique
+			// by default, abbreviation parser generates all unlinked (un-mirrored)
+			// tabstops as ${0}, so we have upgrade all caret tabstops with unique
+			// positions but make sure that all other tabstops are not linked accidentally
+			//value = pyPreprocessText(value);
+            var cur_pos = this.getCaretPos();
+            var index = value.indexOf('${0}');
+            //
+            value = require('utils').replaceUnescapedSymbol(value, '${0}', '');
+            var scintilla = getScintilla();
+            scintilla.replace_sel(value);
+            this.setCaretPos(cur_pos+index);
+		},
+
+		getContent: function() {
+			var scintilla = getScintilla();
+			return scintilla.get_contents(scintilla.get_length()+1);
+
+		},
+
+		getSyntax: function() {
+			return pyGetSyntax();
+		},
+
+		getProfileName: function() {
+			return pyDetectProfile();
+		},
+
+		prompt: function(title) {
+            if (_.isUndefined(title)){
+                var title = 'Enter Abbreviation'; 
+            }
+			return prompt(title);
+		},
+
+		getSelection: function() {
+			var scintilla = getScintilla();
+			return scintilla.get_selection_contents();
+		},
+
+		getFilePath: function() {
+			return cur_doc.file_name;
+		}
+	};
+});
+
+var _completions = {};
+
+function require(name) {
+	return emmet.require(name);
+}
+
+function pyExpandAbbreviationAsYouType(abbr) {
+	var info = require('editorUtils').outputInfo(editorProxy);
+	try {
+		var result = emmet.expandAbbreviation(abbr, info.syntax, info.profile, 
+					require('actionUtils').captureContext(editorProxy));
+		return pyPreprocessText(result);
+	} catch (e) {
+		return '';
+	}
+	
+}
+
+function pyWrapAsYouType(abbr, content) {
+	var info = require('editorUtils').outputInfo(editorProxy);
+	content = require('utils').escapeText(content);
+	var ctx = require('actionUtils').captureContext(editorProxy);
+	try {
+		var result = require('wrapWithAbbreviation').wrap(abbr, content, info.syntax, info.profile, ctx);
+		return pyPreprocessText(result);
+	} catch(e) {
+		return '';
+	}
+}
+
+function pyCaptureWrappingRange() {
+	var info = require('editorUtils').outputInfo(editorProxy);
+	var range = editorProxy.getSelectionRange();
+	var startOffset = range.start;
+	var endOffset = range.end;
+	
+	if (startOffset == endOffset) {
+		// no selection, find tag pair
+		var match = require('htmlMatcher').find(info.content, startOffset);
+		if (!match) {
+			// nothing to wrap
+			return null;
+		}
+		
+		/** @type Range */
+		var utils = require('utils');
+		var narrowedSel = utils.narrowToNonSpace(info.content, match.range);
+		startOffset = narrowedSel.start;
+		endOffset = narrowedSel.end;
+	}
+
+	return [startOffset, endOffset];
+}
+
+function pyGetTagNameRanges(pos) {
+	var ranges = [];
+	var info = require('editorUtils').outputInfo(editorProxy);
+		
+	// search for tag
+	try {
+		var tag = require('htmlMatcher').tag(info.content, pos);
+		if (tag) {
+			var open = tag.open.range;
+			var tagName = /^<([\w\-\:]+)/i.exec(open.substring(info.content))[1];
+			ranges.push([open.start + 1, open.start + 1 + tagName.length]);
+
+			if (tag.close) {
+				ranges.push([tag.close.range.start + 2, tag.close.range.start + 2 + tagName.length]);
+			}
+		}
+	} catch (e) {}
+
+	return ranges;
+}
+
+function pyGetTagRanges() {
+	var ranges = [];
+	var info = require('editorUtils').outputInfo(editorProxy);
+		
+	// search for tag
+	try {
+		var tag = require('htmlMatcher').tag(info.content, editorProxy.getCaretPos());
+		if (tag) {
+			ranges.push(tag.open.range.toArray());
+			if (tag.close) {
+				ranges.push(tag.close.range.toArray());
+			}
+		}
+	} catch (e) {}
+
+	return ranges;
+}
+
+function pyExtractAbbreviation() {
+	return require('expandAbbreviation').findAbbreviation(editorProxy);
+}
+
+function pyHasSnippet(name) {
+	return !!emmet.require('resources').findSnippet(editorProxy.getSyntax(), name);
+}
+
+/**
+ * Get all available CSS completions. This method is optimized for CSS
+ * only since it should contain snippets only so it's not required
+ * to do extra parsing
+ */
+function pyGetCSSCompletions(dialect) {
+	dialect = dialect || pyGetSyntax();
+
+	if (!_completions[dialect]) {
+		var all = require('resources').getAllSnippets(dialect);
+		var css = require('cssResolver');
+		_completions[dialect] = _.map(all, function(v, k) {
+			var snippetValue = typeof v.parsedValue == 'object' 
+				? v.parsedValue.data
+				: v.value;
+			var snippet = css.transformSnippet(snippetValue, false, dialect);
+			return {
+				k: v.nk,
+				label: snippet.replace(/\:\s*\$\{0\}\s*;?$/, ''),
+				v: css.expandToSnippet(v.nk, dialect)
+			};
+		});
+	}
+
+	return _completions[dialect];
+}
+
+/**
+ * Returns current syntax name
+ * @return {String}
+ */
+function pyGetSyntax() {
+	var scope = cur_doc_type;
+    var syntax = 'html'
+    if (scope == 'xml' || scope == 'xsl') {
+		syntax = 'xsl';
+    }
+    if(scope == 'css' || scope == 'less' || scope == 'sass'){
+        syntax = 'css'
+    }
+    return require('actionUtils').detectSyntax(editorProxy, syntax);
+}
+
+function pyDetectProfile(argument) {
+	return require('actionUtils').detectProfile(editorProxy);
+}
---
+++ geanypy-emmet.py
@@ -0,0 +1,125 @@
+import os
+from gettext import gettext as _
+from gi.repository import Gtk
+import geany
+import emmet
+from emmet.context import Context
+
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+#~ _key_mod = namedtuple("KeyMod", 'KEY MOD')
+#~ 
+#~ _actions = { "expand_abbreviation": _key_mod(Gdk.KEY_e, Gdk.ModifierType.SHIFT_MASK | Gdk.ModifierType.CONTROL_MASK),
+	 #~ "expand_abbreviation_with_tab": _key_mod(Gdk.KEY_T, Gdk.ModifierType.SHIFT_MASK | Gdk.ModifierType.CONTROL_MASK),
+	 #~ "match_pair_inward": _key_mod(Gdk.KEY_L, Gdk.ModifierType.SHIFT_MASK | Gdk.ModifierType.CONTROL_MASK),
+	 #~ "match_pair_outward": _key_mod(Gdk.KEY_R, Gdk.ModifierType.SHIFT_MASK | Gdk.ModifierType.CONTROL_MASK),
+	 #~ "wrap_with_abbreviation": _key_mod(Gdk.KEY_q, Gdk.ModifierType.SHIFT_MASK | Gdk.ModifierType.CONTROL_MASK),
+	 #~ "prev_edit_point": _key_mod(Gdk.KEY_p, Gdk.ModifierType.SHIFT_MASK | Gdk.ModifierType.CONTROL_MASK),
+	 #~ "next_edit_point": _key_mod(Gdk.KEY_n, Gdk.ModifierType.SHIFT_MASK | Gdk.ModifierType.CONTROL_MASK),
+	 #~ "insert_formatted_newline": _key_mod(Gdk.KEY_l, Gdk.ModifierType.SHIFT_MASK | Gdk.ModifierType.CONTROL_MASK),
+	 #~ "select_line": _key_mod(Gdk.KEY_s, Gdk.ModifierType.SHIFT_MASK | Gdk.ModifierType.CONTROL_MASK),
+	 #~ "go_to_matching_pair": _key_mod(Gdk.KEY_m, Gdk.ModifierType.SHIFT_MASK | Gdk.ModifierType.CONTROL_MASK),
+	 #~ "merge_lines": _key_mod(Gdk.KEY_b, Gdk.ModifierType.SHIFT_MASK | Gdk.ModifierType.CONTROL_MASK),
+	 #~ "toggle_comment": _key_mod(Gdk.KEY_c, Gdk.ModifierType.SHIFT_MASK | Gdk.ModifierType.CONTROL_MASK),
+	 #~ "split_join_tag": _key_mod(Gdk.KEY_j, Gdk.ModifierType.SHIFT_MASK | Gdk.ModifierType.CONTROL_MASK),
+	 #~ "remove_tag": _key_mod(Gdk.KEY_r, Gdk.ModifierType.SHIFT_MASK | Gdk.ModifierType.CONTROL_MASK),
+	 #~ "increment_number_by_1": _key_mod(0, 0),
+	 #~ "increment_number_by_10": _key_mod(0, 0),
+	 #~ "increment_number_by_01": _key_mod(0, 0),
+	 #~ "decrement_number_by_1": _key_mod(0, 0),
+	 #~ "decrement_number_by_10": _key_mod(0, 0),
+	 #~ "decrement_number_by_01": _key_mod(0, 0),
+	#~ "evaluate_math_expression": _key_mod(0, 0)
+    #~ }
+actions = ("expand_abbreviation", "match_pair_inward", "match_pair_outward","wrap_with_abbreviation", "prev_edit_point", "next_edit_point", "insert_formatted_newline", "select_previous_item", "select_next_item", "go_to_matching_pair", "merge_lines", "toggle_comment", "split_join_tag", "remove_tag", "update_image_size", "evaluate_math_expression", "reflect_css_value", "insert_formatted_line_break_only", "encode_decode_data_url", "increment_number_by_1", "increment_number_by_10", "increment_number_by_01", "decrement_number_by_1", "decrement_number_by_10", "decrement_number_by_01")
+
+def create_action_label():
+    #labels = tuple()
+    for action in actions:
+        if action == "match_pair_outward":
+            action = _("Match Tag Outward")
+        elif action == "match_pair_inward":
+            action = _("Match Tag Inward")
+        elif action == "increment_number_by_01":
+            action = _("Increment Number by 0.1")
+        elif action == "decrement_number_by_01":
+            action = _("Decrement Number by 0.1")
+        elif action == "prev_edit_point":
+            action = _("Previous Edit Point")
+        elif action == "split_join_tag":
+            action = _("Split or Join Tag")
+        elif action == "encode_decode_data_url":
+            action = _("Encode/decode image to data:URL")
+        else:
+            action = _(action.replace("_", " ").title())
+        yield action
+        #labels += (action,)
+    #return labels
+
+actions_dict = dict(zip([label for label in create_action_label()], actions))
+
+class EmmetPlugin(geany.Plugin):
+
+    __plugin_name__ = "Emmet"
+    __plugin_version__ = "0.1"
+    __plugin_description__ = "Emmet Plugin for geany."
+    __plugin_author__ = "Sagar Chalise <chalisesagar@gmail.com>"
+
+    def __init__(self):
+        self.menu_item = Gtk.MenuItem(_("Emmet"))
+        imenu = Gtk.Menu()
+        for label in create_action_label():
+            menu_item = Gtk.MenuItem(label)
+            menu_item.connect("activate", self.on_action_item_activate, actions_dict[label])
+            menu_item.show()
+            imenu.append(menu_item)
+            try:
+                geany.bindings.register_binding("Emmet", label, self.on_key_activate, actions_dict[label])
+            except AttributeError:
+                geany.ui_utils.set_statusbar("GeanyPy was not compiled with keybindings support.")
+        self.menu_item.set_submenu(imenu)
+        self.menu_item.show()
+        geany.main_widgets.tools_menu.append(self.menu_item)
+        
+
+    def cleanup(self):
+        self.menu_item.destroy()
+
+    @staticmethod
+    def prompt(title):
+        dialog = Gtk.Dialog(title, geany.main_widgets.window, Gtk.DialogFlags.DESTROY_WITH_PARENT or Gtk.DialogFlags.MODAL, (Gtk.STOCK_CANCEL, Gtk.ResponseType.REJECT,
+             Gtk.STOCK_OK, Gtk.ResponseType.ACCEPT))
+        dialog.set_default_size(300, -1)
+        dialog.set_default_response(Gtk.ResponseType.ACCEPT)
+        content_area = dialog.get_content_area()
+        entry = Gtk.Entry()
+        vbox = Gtk.VBox(False, 0)
+        vbox.pack_start(entry, True, True, 0)
+        vbox.set_border_width(12)
+        content_area.add(vbox)
+        vbox.show_all()
+        response = dialog.run()
+        abbr = ''
+        if response == Gtk.ResponseType.ACCEPT:
+            abbr = entry.get_text()
+        dialog.destroy()
+        return abbr
+
+    @staticmethod
+    def run_emmet_action(action):
+        file_types = ('HTML', 'PHP', 'XML', 'CSS')
+        cur_file_type = geany.document.get_current().file_type.name
+        if cur_file_type in file_types:
+            contrib = {
+                'cur_doc': geany.document.get_current(),
+                'cur_doc_type': cur_file_type.lower() if cur_file_type != 'PHP' else 'html',
+                'prompt': EmmetPlugin.prompt
+            }
+            ctx = Context(files=[os.path.join(BASE_PATH, 'editor.js')], contrib=contrib)
+            with ctx.js() as c:
+                c.locals.pyRunAction(action)
+
+    def on_action_item_activate(self, data, action_key):
+        self.run_emmet_action(action_key)
+        
+    def on_key_activate(self, key_id, name):
+        self.run_emmet_action(name)
