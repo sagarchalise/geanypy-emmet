73003c5919d6e4bee45b9be815db8d42cf1ac73a
Sagar Chalise <chalisesagar@gmail.com>
2013-08-06 13:29:39 +0545

    Used as snippet and cursor placeholder added

--- editor.js
+++ editor.js
@@ -1,9 +1,17 @@
+function pySetupEditorProxy() {
+	editorProxy._syntax = cur_doc_type;
+    require('utils').setCaretPlaceholder("%cursor%");
+	var indentation = "\t";
+	var nl = cur_doc.editor.eol_char;
+	require('resources').setVariable('indentation', indentation);
+	require('resources').setVariable('newline', nl);
+}
 function getScintilla() {
 	return cur_doc.editor.scintilla;
 }
 
 var editorProxy = emmet.exec(function(require, _) {
-	return {
+    return {
 		getSelectionRange: function() {
 			var scintilla = getScintilla();
 			return {
@@ -47,28 +55,17 @@ var editorProxy = emmet.exec(function(require, _) {
 				end = _.isUndefined(start) ? this.getContent().length : start;
 			if (_.isUndefined(start)) start = 0;
             var sel_range = this.getSelectionRange();
-            //
-            var index = value.indexOf('{0}');
-            var tabstopData = require('tabStops').extract(value, {
-				escape: function(ch) {
-					return ch;
-				}
-			});
-            value = tabstopData.text;
-            var firstTabStop = tabstopData.tabstops[0];
-            if(!firstTabStop){
-                firstTabStop= {
-                        start: index-1,
-                        end: index
-                    }
-            }
             if (sel_range.start === sel_range.end){
                 this.createSelection(start, end);
             }
+            // indent new value
+			if (!noIndent) {
+                var utils = require('utils');
+				value = utils.padString(value, utils.getLinePadding(this.getCurrentLine()));
+			}
             var scintilla = getScintilla();
-            scintilla.replace_sel(value);
-            var cur_pos = (start <= end) ? start+firstTabStop.start : end+firstTabStop.end;
-            this.setCaretPos(cur_pos);
+            scintilla.replace_sel("");
+            cur_doc.editor.insert_snippet(start, value);
 		},
 
 		getContent: function() {
@@ -78,7 +75,7 @@ var editorProxy = emmet.exec(function(require, _) {
 		},
 
 		getSyntax: function() {
-			return pyGetSyntax();
+			return require('actionUtils').detectSyntax(this, cur_doc_type);;
 		},
 
 		getProfileName: function() {
@@ -106,22 +103,6 @@ var editorProxy = emmet.exec(function(require, _) {
 function require(name) {
 	return emmet.require(name);
 }
-/**
- * Returns current syntax name
- * @return {String}
- */
-function pyGetSyntax() {
-	var scope = cur_doc_type;
-    var syntax = 'html'
-    if (scope == 'xml' || scope == 'xsl') {
-		syntax = scope;
-    }
-    if(scope == 'css' || scope == 'less' || scope == 'sass'){
-        syntax = 'css'
-    }
-    return require('actionUtils').detectSyntax(editorProxy, syntax);
-}
-
 function pyDetectProfile(argument) {
 	return require('actionUtils').detectProfile(editorProxy);
 }
