b98f68a5a12a12b6862286763488a6f466aa75da
Sagar Chalise <chalisesagar@gmail.com>
2016-10-26 08:23:39 +0545

    master merge

--- editor.js
+++ editor.js
@@ -8,11 +8,17 @@ var tabStops = emmet.tabStops;
 
 function pySetupEditorProxy() {
     editorProxy._syntax = cur_doc_type;
-    utils.setCaretPlaceholder("%cursor%");
+    var indentation = '\t';
+	if (!tab_used) {
+		indentation = utils.repeatString(' ', +tab_width);
+	}
+
     resources.setVariable('newline', cur_doc.editor.eol_char);
+    resources.setVariable('indentation', indentation);
+    utils.setCaretPlaceholder("%cursor%");
 
     var matchPairHighlight = function(editor) {
-    	var content = String(editor.getContent());
+    var content = String(editor.getContent());
 	var caretPos = editor.getCaretPos();
 	if (content.charAt(caretPos) == '<')
             // looks like caret is outside of tag pair
@@ -40,85 +46,86 @@ function getScintilla() {
 }
 var editorProxy = {
     getSelectionRange: function() {
-	var scintilla = getScintilla();
-	return {
-	    start: scintilla.get_selection_start(),
-	    end: scintilla.get_selection_end()
-	};
+        var scintilla = getScintilla();
+        return {
+            start: scintilla.get_selection_start(),
+            end: scintilla.get_selection_end(),
+        };
     },
     createSelection: function(start, end) {
-	var scintilla = getScintilla();
-	scintilla.set_selection_start(start);
-	scintilla.set_selection_end(end);
+        if (typeof end === 'undefined') {
+				end = start;
+        }
+        var scintilla = getScintilla();
+        scintilla.set_selection_start(start);
+        scintilla.set_selection_end(end);
     },
     getCurrentLineRange: function() {
-	var scintilla = getScintilla();
-	var line = scintilla.get_current_line();
-	return {
-	    start: scintilla.get_position_from_line(line),
-	    end: scintilla.get_line_end_position(line)
-	};
+        var scintilla = getScintilla();
+        var line = scintilla.get_current_line();
+        return {
+            start: scintilla.get_position_from_line(line),
+            end: scintilla.get_line_end_position(line)
+        };
     },
     getCaretPos: function() {
-	var scintilla = getScintilla();
-	return scintilla.get_current_position();
+        var scintilla = getScintilla();
+        return scintilla.get_current_position();
     },
     setCaretPos: function(pos){
-	var scintilla = getScintilla();
-	scintilla.set_current_position(pos, true);
+        var scintilla = getScintilla();
+        scintilla.set_current_position(pos, true);
     },
     getCurrentLine: function() {
-	var scintilla = getScintilla();
-	return scintilla.get_line(scintilla.get_current_line());
+        var scintilla = getScintilla();
+        return scintilla.get_line(scintilla.get_current_line());
     },
     replaceContent: function(value, start, end, noIndent) {
-	if (typeof end === 'undefined')
-	    end = typeof start === 'undefined' ? this.getContent().length : start;
-	if (typeof start === 'undefined') start = 0;
-            var sel_range = this.getSelectionRange();
-	if (sel_range.start === sel_range.end){
-	    this.createSelection(start, end);
-	}
-	var scintilla = getScintilla();
-	scintilla.replace_sel("");
-    value = pyPreprocessText(value);
-    value = editorUtils.normalize(value);
-	cur_doc.editor.insert_snippet(start, value);
+        if (typeof end === 'undefined')
+            end = typeof start === 'undefined' ? this.getContent().length : start;
+        if (typeof start === 'undefined') start = 0;
+                var sel_range = this.getSelectionRange();
+        if (sel_range.start === sel_range.end){
+            this.createSelection(start, end);
+        }
+        var scintilla = getScintilla();
+        scintilla.replace_sel("");
+        value = pyPreprocessText(value);
+        value = editorUtils.normalize(value);
+        cur_doc.editor.insert_snippet(start, value);
     },
     getContent: function() {
-	var scintilla = getScintilla();
-	return scintilla.get_contents(scintilla.get_length()+1);
+        return cur_content;
     },
     getSyntax: function() {
-	return actionUtils.detectSyntax(this, cur_doc_type);;
+        return actionUtils.detectSyntax(this, cur_doc_type);;
     },
     getProfileName: function() {
-	return pyDetectProfile();
+        return pyDetectProfile();
     },
     prompt: function(title) {
-	if (typeof title === 'undefined'){
-	    var title = 'Enter Abbreviation';
-	}
-	return prompt(title);
+        if (typeof title === 'undefined'){
+            var title = 'Enter Abbreviation';
+        }
+        return prompt(title);
     },
     getSelection: function() {
-	var scintilla = getScintilla();
-	return scintilla.get_selection_contents();
+        return cur_selection;
     },
     getFilePath: function() {
-	return cur_doc.file_name;
+        return cur_doc.file_name;
     },
     setIndicator: function(range){
-	var geanyIndicators = [geanyIndicatorSearch, 1];
-	for(var i=0;i<geanyIndicators.length;i++){
-	    var start = range.start;
-	    var stop = range.end;
-	    if(i == 1){
-		start += 1;
-		stop -= 1;
-	    }
-	    cur_doc.editor.indicator_set_on_range(geanyIndicators[i], start, stop);
-	}
+        var geanyIndicators = [geanyIndicatorSearch, 1];
+        for(var i=0;i<geanyIndicators.length;i++){
+            var start = range.start;
+            var stop = range.end;
+            if(i == 1){
+                start += 1;
+                stop -= 1;
+            }
+            cur_doc.editor.indicator_set_on_range(geanyIndicators[i], start, stop);
+        }
     },
 };
 
